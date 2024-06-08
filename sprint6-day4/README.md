86d9c68db6f5795f01c3d13619a48ddf9dbea470
A. Ulaş Çankaya <aulascankaya@gmail.com>
2024-06-08 12:47:53 +0300

    day4 project added

---
+++ sprint6-day4/README.md
@@ -0,0 +1,4 @@
+Css in JavaScript  and  React Component Libraries
+
+-Offer.jsx (React Component Lib. , Reactstrap)
+-Product.jsx (Css in JavaScript,Styled Components)
---
+++ sprint6-day4/src/components/Offer.jsx
@@ -0,0 +1,73 @@
+/* ADIM 1: Reactstrap Carousel'i burada oluşturabilirsin */
+import { sahteVeri } from '../sahteVeri.js';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+
+export default function Offer(args) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === sahteVeri.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === 0 ? sahteVeri.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = sahteVeri.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.title} />
+        <CarouselCaption captionText={item.text} captionHeader={item.title} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      {...args}
+    >
+      <CarouselIndicators
+        items={sahteVeri}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+}
